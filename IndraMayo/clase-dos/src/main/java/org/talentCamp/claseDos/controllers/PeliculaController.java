4a1a0aacab0cc7f001b2fa9beca4f0edcd0bad43
Esteban Calabria <esteban.calabria@gmail.com>
2024-05-07 05:52:51 -0300

    clase-tres-spring

--- IndraMayo/clase-dos/src/main/java/org/talentCamp/claseDos/controllers/PeliculaController.java
+++ IndraMayo/clase-dos/src/main/java/org/talentCamp/claseDos/controllers/PeliculaController.java
@@ -1,6 +1,10 @@
 package org.talentCamp.claseDos.controllers;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.*;
 import org.talentCamp.claseDos.models.Pelicula;
@@ -31,5 +35,17 @@ public class PeliculaController {
     @CrossOrigin("http://localhost:4200")
     public List<Pelicula> todas(){
         return this.service.obtenerTodas();
+    }
+
+    @DeleteMapping("/api/pelicula/{id}")
+    @CrossOrigin("http://localhost:4200")
+    public ResponseEntity<Boolean> delete(@PathVariable("id") int id){
+        try{
+            this.service.borrarPelicula(id);
+            return ResponseEntity.ok(true);
+        } catch (Exception e) {
+            //return ResponseEntity.notFound().build();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
